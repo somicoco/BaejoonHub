e9aa0f8fd2d4fc427b6793719ff0bbd67667ebb7
somicoco <111287987+somicoco@users.noreply.github.com>
2022-08-23 19:58:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 332 ms, Memory: 18032 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+
+		 for(int i=1;i<=T;i++) {
+			 for(int j=1;j<=i;j++) {
+				 System.out.print("*");		
+			 }
+			 System.out.println("");	
+		  }
+		   }
+		 
+}
