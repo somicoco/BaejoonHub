644ae3e781bfbd12df6a823876b6a07800dcf3d7
somicoco <111287987+somicoco@users.noreply.github.com>
2022-09-01 22:03:24 +0900

    [Bronze II] Title: OX퀴즈, Time: 228 ms, Memory: 17840 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		String arr[] = new String[n+1];
+		String arr[] = new String[n];
 		
 		
 		for(int i =0; i<n;i++) {
