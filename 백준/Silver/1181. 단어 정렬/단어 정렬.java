0af56df143bcdcc76f8b523a0fe57e90cbc6eeff
ShinDasom <111287987+somicoco@users.noreply.github.com>
2023-07-10 00:48:23 +0900

    [Silver V] Title: 단어 정렬, Time: 580 ms, Memory: 23612 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,46 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
-
+    static ArrayList<String> list = new ArrayList<>();
+    static HashSet<String> hashSet = new HashSet<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        List<String> list = new ArrayList<>();
-
-        for(int i=0; i<n; i++){
-            list.add(br.readLine());
-        }
-
-
-        Set<String> set = new HashSet<String>(list);
-        List<String> newList = new ArrayList<String>(set);
-
+        int num = Integer.parseInt(br.readLine());
         Comparator<String> c = new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length()==o2.length()){
+                if(o1.length()==o2.length()){
                     return o1.compareTo(o2);
                 }
                 else{
                     return o1.length()-o2.length();
                 }
-
             }
         };
 
-        Collections.sort(newList,c);
-
-        for (String s:newList
-             ) {
-            System.out.println(s);
-            
+        for(int i=0;i<num;i++){
+            list.add(br.readLine());
         }
+        Set<String> set= new HashSet<>(list);
+        List<String> newlist = new ArrayList<>(set);
 
+       Collections.sort(newlist,c);
 
+        for(String s : newlist){
+            System.out.println(s);
+        }
     }
 }
