43b8a39e817d9246c6e1ff82fbd3edbf3610dd99
ShinDasom <111287987+somicoco@users.noreply.github.com>
2023-08-03 00:21:54 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 352 ms, Memory: 20004 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,44 +1,62 @@
-import java.util.Scanner;
-import java.util.Stack;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s;
 
-        while (true) {
-            s = scan.nextLine();
-            if (s.equals(".")) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            String s = br.readLine();
+            if(s.equals(".")) {
                 break;
-            } else
-                System.out.println(solve(s));
-        }
-    }
+            }else {
+                String result;
+                Stack<Character> stack = new Stack<>();
+                for (int i =0; i<s.length();i++){
+                    char c = s.charAt(i);
+                    if (c == '(' || c == '[')
+                        stack.push(c);
 
-    private static String solve(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(' || c == '[')
-                stack.push(c);
+                    else if (c == ')') {
+                        if (!stack.isEmpty()&&stack.peek() == '(') {
+                            stack.pop();
 
-            else if (c == ')') {
-                if (stack.isEmpty() || stack.peek() != '(')
-                    return "no";
+                        }
+                        else{
+                            stack.push(c);
+                        }
+
+
+                    } else if (c == ']') {
+                        if (!stack.isEmpty()&&stack.peek() == '[') {
+                            stack.pop();
+                        }
+                        else{
+                            stack.push(c);
+                        }
+                    }
+                }
+                if (stack.isEmpty()&&s.charAt(s.length()-1)=='.'){
+                    result = "yes";
+
+                }
                 else
-                    stack.pop();
-            } else if (c == ']') {
-                if (stack.isEmpty() || stack.peek() != '[')
-                    return "no";
-                else
-                    stack.pop();
+                    result = "no";
+
+                System.out.println(result);
+
             }
+
+
         }
 
-        if (stack.isEmpty())
-            return "yes";
-        else
-            return "no";
+
+
+        }
+
     }
-}
