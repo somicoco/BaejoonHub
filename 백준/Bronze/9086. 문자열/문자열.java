330a34efe94ff3aedf8956a7275a143fbbd26a37
ShinDasom <zaqaziq@naver.com>
2023-10-04 23:53:22 +0900

    [Bronze V] Title: 문자열, Time: 204 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		String[] arr = new String[T];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.next();
+		}
+		sc.close();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i].length() < 1) {
+				System.out.println(arr[i] + "" + arr[i]);
+			} else {
+				System.out.print(arr[i].substring(0, 1)); // 0번째 1번째 출력
+				System.out.println((arr[i].substring(arr[i].length() - 1, arr[i].length())));// 끝에서 -1번째부터 끝번째까지
+			}
+
+		}
+	}
+
+}
