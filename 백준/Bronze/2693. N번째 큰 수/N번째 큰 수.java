179d80fd58bf4524864ded7c4d6529aadd01d213
ShinDasom <zaqaziq@naver.com>
2023-11-04 23:54:46 +0900

    [Bronze I] Title: N번째 큰 수, Time: 184 ms, Memory: 16004 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());  
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int[] arr = new int[10];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);  
+            sb.append(arr[7] + "\n");  
+        }
+        System.out.print(sb);
+    }
+}
