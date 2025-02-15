eaee1939f2a251ffeb2f82949a03ca5028353674
ShinDasom <zaqaziq@naver.com>
2025-02-15 14:54:21 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.35 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,28 +1,37 @@
+import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = {};
-        int [] result = new int[targets.length];
+        int[] answer = new int[targets.length];
         
-        for(int i = 0; i<targets.length; i++){
-            for(char targetStr : targets[i].toCharArray()){
-                int min = Integer.MAX_VALUE;
-                for(int j = 0; j<keymap.length;j++){
-                    int cnt = keymap[j].indexOf(Character.toString(targetStr))+1;
-                    if(cnt>0&&cnt<min){
-                        min = cnt;
-                    }
+        Map<Character, Integer> maps = new HashMap<Character, Integer>();
+        
+        for(int i = 0; i<keymap.length;i++){
+            char[] charArr = keymap[i].toCharArray();
+            for(int j = 0; j<charArr.length;j++){
+                // 중복된 값일 경우 최솟값 비교해서 교체
+                if(maps.containsKey(charArr[j])){
+                    maps.replace(charArr[j],Math.min(maps.get(charArr[j]),j+1));
+                    }else{
+                    maps.put(charArr[j],j+1);
                 }
-                if(min == Integer.MAX_VALUE){
-                    result[i] = -1;
+                }
+            }
+        
+         for(int i = 0; i<targets.length;i++){
+            for(char targetStr:targets[i].toCharArray()){
+                if(maps.containsKey(targetStr)){
+                    answer[i] = answer[i]+maps.get(targetStr);
+                }else{
+                    answer[i] = -1;
                     break;
                 }
-               result[i] = result[i]+min;
+                
             }
-            
         }
         
-        answer = result;
-    
+          
         return answer;
     }
 }
+
+        
